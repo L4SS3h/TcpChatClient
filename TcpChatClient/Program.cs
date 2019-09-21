c5757baba7bded61a786e0e4dbd6631703760e2c
Lasse Henriksen <lass5676@edu.easj.dk>
2019-09-21 01:07:54 +0100

    Add project files.

---
+++ TcpChatClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace TcpChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Client side of a TCP chat server.
+            TcpClient clientSocket = new TcpClient("127.0.0.1", 6789);
+            Console.WriteLine("Client started!");
+            NetworkStream ns = clientSocket.GetStream();
+            StreamWriter sw = new StreamWriter(ns);
+            StreamReader st = new StreamReader(ns);
+            sw.AutoFlush = true;
+
+            while (true)
+            {
+                
+                string message = Console.ReadLine();
+                sw.WriteLine(message);
+                string message1 = st.ReadLine();
+                Console.WriteLine("Server: " + message1);
+                
+            }
+            Console.ReadLine();
+            ns.Close();
+            clientSocket.Close();
+
+        }
+    }
+}
